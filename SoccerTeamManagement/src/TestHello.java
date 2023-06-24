e4d07da5d8f600e0d4ddab361d6ed29d0cadf0ff
윤성용 <xnowhere@naver.com>
2023-06-25 01:32:02 +0900

    브랜치 하나 더 테스트

--- SoccerTeamManagement/src/TestHello.java
+++ SoccerTeamManagement/src/TestHello.java
@@ -2,5 +2,6 @@ public class TestHello {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("브랜치 추가함");
+		System.out.println("브랜치 하나 더 테스트");
 	}
 }
