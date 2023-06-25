f95a08afa72abc531143f1e595e8e72826168af2
정욱진 <user@58.235.137.35>
2023-06-25 21:52:23 +0900

    욱진 집에서 테스트 중입니다.

--- SoccerTeamManagement/src/TestHello.java
+++ SoccerTeamManagement/src/TestHello.java
@@ -3,5 +3,6 @@ public class TestHello {
 		System.out.println("HelloWorld");
 		System.out.println("브랜치 추가함");
 		System.out.println("브랜치 하나 더 테스트");
+		System.out.println("욱진 집에서 테스트 해봤습니다.");
 	}
 }
