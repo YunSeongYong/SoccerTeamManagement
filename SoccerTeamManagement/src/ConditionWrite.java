44ace5a694e03968e3c7139ed2514851e1abff67
윤성용 <xnowhere@naver.com>
2023-06-26 12:43:46 +0900

    기능 클래스 추가

---
+++ SoccerTeamManagement/src/ConditionWrite.java
@@ -0,0 +1,9 @@
+
+public class ConditionWrite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SoccerTeamManagement/src/DoctorAppointment.java
@@ -0,0 +1,9 @@
+
+public class DoctorAppointment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
