1657ace0719b07bf9f9a626cb51155a021e4012e
GGG <GGG@DESKTOP-8N3GG4O>
2023-06-29 12:45:58 +0900

    코치 로그인 탭 중 예약하기 탭 구현했습니다.

--- SoccerTeamManagement/src/DoctorAppointment.java
+++ SoccerTeamManagement/src/DoctorAppointment.java
@@ -1,9 +1,125 @@
-
 public class DoctorAppointment {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int backnumber;
+	private String playerName;
+	private String date;
+	private String time;
+	private String condition;
+	private String doctor;
+	
+	public DoctorAppointment(int backnumber, String playerName, String date, String time, String condition,
+			String doctor) {
+		this.backnumber = backnumber;
+		this.playerName = playerName;
+		this.date = date;
+		this.time = time;
+		this.condition = condition;
+		this.doctor = doctor;
 	}
 
+	public int getBacknumber() {
+		return backnumber;
+	}
+
+	public void setBacknumber(int backnumber) {
+		this.backnumber = backnumber;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public String getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(String doctor) {
+		this.doctor = doctor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + backnumber;
+		result = prime * result + ((condition == null) ? 0 : condition.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((doctor == null) ? 0 : doctor.hashCode());
+		result = prime * result + ((playerName == null) ? 0 : playerName.hashCode());
+		result = prime * result + ((time == null) ? 0 : time.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DoctorAppointment other = (DoctorAppointment) obj;
+		if (backnumber != other.backnumber)
+			return false;
+		if (condition == null) {
+			if (other.condition != null)
+				return false;
+		} else if (!condition.equals(other.condition))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (doctor == null) {
+			if (other.doctor != null)
+				return false;
+		} else if (!doctor.equals(other.doctor))
+			return false;
+		if (playerName == null) {
+			if (other.playerName != null)
+				return false;
+		} else if (!playerName.equals(other.playerName))
+			return false;
+		if (time == null) {
+			if (other.time != null)
+				return false;
+		} else if (!time.equals(other.time))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "DoctorAppointment [backnumber=" + backnumber + ", playerName=" + playerName + ", date=" + date
+				+ ", time=" + time + ", condition=" + condition + ", doctor=" + doctor + "]";
+	}
+	
 }
--- SoccerTeamManagement/src/StaffRegistration.java
+++ SoccerTeamManagement/src/StaffRegistration.java
@@ -1,5 +1,422 @@
-public class StaffRegistration {
-	public static void main(String[] args) {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.*;
+import javax.swing.event.*;
+
+import dbutil.DBUtil;
+import java.awt.Component;
+import javax.swing.table.TableModel;
+import javax.swing.table.DefaultTableModel;
+
+public class StaffRegistration extends JFrame implements ChangeListener {
+   JTabbedPane pane;
+   private JLabel lblNewLabel;
+   private JTextField textField;
+   private JTextField textField_1;
+   private JTextField textField_2;
+   private JTextField textField_3;
+   JComboBox<String> comboBox;
+   JComboBox<LocalDate> comboBox_1;
+   private JTable table;
+   private DefaultTableModel tableModel;
+   private List<DoctorAppointment> list2 = new ArrayList<>();
+   private JTextField textField_4;
+
+   public List<DoctorAppointment> selectList(String doctor) {
+	   list2 = new ArrayList<>();
+	   Connection conn = null;
+	   PreparedStatement stmt = null;
+	   ResultSet rs = null;
+	   try {
+		conn = DBUtil.getConnection();
+		stmt = conn.prepareStatement("SELECT * FROM project_test.appointment where doctor = ?;");
+		stmt.setString(1, doctor);
+		rs = stmt.executeQuery();
+		
+		while (rs.next()) {
+			int number = rs.getInt("number");
+			String name = rs.getString("name");
+			String date = rs.getString("date");
+			String time = rs.getString("time");
+			String condition = rs.getString("condition");
+			String doctor2 = rs.getString("doctor");
+			
+			System.out.printf("%d, %s, %s, %s, %s, %s", number, name, time, date, condition, doctor);
+			list2.add(new DoctorAppointment(number, name, date, time ,condition, doctor));
+			System.out.println(list2);
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		DBUtil.close(rs);
+		DBUtil.close(stmt);
+		DBUtil.close(conn);		
 	}
+	return list2;
+			   
+   }
+   
+   public void doctorSchedule(List<DoctorAppointment> list2) {
+	    DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+
+	    // Clear the existing table data
+	    tableModel.setRowCount(0);
+
+	    // Add the appointments to the table
+	    for (DoctorAppointment appointment : list2) {
+	        Object[] rowData = {
+	                appointment.getBacknumber(),
+	                appointment.getPlayerName(),
+	                appointment.getDate(),
+	                appointment.getTime(),
+	                appointment.getCondition(),
+	                appointment.getDoctor()
+	        };
+	        tableModel.addRow(rowData);
+	    }
+	}
+	   
+   public void displayDoctorSchedule() {
+	    String selectedDate = comboBox_1.getSelectedItem().toString(); 
+	    String selectedDoctor = comboBox.getSelectedItem().toString(); 
+
+	    Connection conn = null;
+	    PreparedStatement stmt = null;
+	    ResultSet rs = null;
+
+	    try {
+	        conn = DBUtil.getConnection();
+
+	        // Prepare the SQL query to retrieve appointments for the selected date and doctor
+	        String query = "SELECT * FROM appointment WHERE date = ? AND doctor = ?";
+	        stmt = conn.prepareStatement(query);
+	        stmt.setString(1, selectedDate);
+	        stmt.setString(2, selectedDoctor);
+
+	        rs = stmt.executeQuery();
+
+	        list2.clear(); // Clear the existing list of appointments
+
+	        while (rs.next()) {
+	            int number = rs.getInt("number");
+	            String name = rs.getString("name");
+	            String date = rs.getString("date");
+	            String time = rs.getString("time");
+	            String condition = rs.getString("condition");
+	            String doctor = rs.getString("doctor");
+
+	            // Print appointment details for testing
+	            System.out.printf("%d %s %s %s %s %s\n", number, name, date, time, condition, doctor);
+
+	            // Add the appointment to the list
+	            list2.add(new DoctorAppointment(number, name, date, time, condition, doctor));
+	        }
+
+	        doctorSchedule(list2); // Update the table with the retrieved appointments
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        DBUtil.close(rs);
+	        DBUtil.close(stmt);
+	        DBUtil.close(conn);
+	    }
+	}
+   
+   public void displaySchedule() {
+	    String selectedDate = comboBox_1.getSelectedItem().toString();
+	    String selectedDoctor = comboBox.getSelectedItem().toString();
+
+	    Connection conn = null;
+	    PreparedStatement stmt = null;
+	    ResultSet rs = null;
+
+	    try {
+	        conn = DBUtil.getConnection();
+
+	        // Prepare the SQL query to retrieve appointments for the selected date and doctor
+	        String query = "SELECT * FROM appointment WHERE date = ? AND doctor = ?";
+	        stmt = conn.prepareStatement(query);
+	        stmt.setString(1, selectedDate);
+	        stmt.setString(2, selectedDoctor);
+
+	        rs = stmt.executeQuery();
+
+	        list2.clear(); // Clear the existing list of appointments
+
+	        while (rs.next()) {
+	            int number = rs.getInt("number");
+	            String name = rs.getString("name");
+	            String date = rs.getString("date");
+	            String time = rs.getString("time");
+	            String condition = rs.getString("condition");
+	            String doctor = rs.getString("doctor");
+
+	            // Print appointment details for testing
+	            System.out.printf("%d %s %s %s %s %s\n", number, name, date, time, condition, doctor);
+
+	            // Add the appointment to the list
+	            list2.add(new DoctorAppointment(number, name, date, time, condition, doctor));
+	        }
+
+	        doctorSchedule(list2); // Update the table with the retrieved appointments
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        DBUtil.close(rs);
+	        DBUtil.close(stmt);
+	        DBUtil.close(conn);
+	    }
+	}
+   
+   
+   public void insertAppointment() {
+	    Connection conn = null;
+	    PreparedStatement stmt = null;
+
+	    try {
+	        conn = DBUtil.getConnection();
+
+	        // 의사 콤보박스에서 선택된 값을 가져옵니다
+	        String selectedDoctor = (String) comboBox.getSelectedItem();
+
+	        stmt = conn.prepareStatement("insert into appointment(number, name, date, time,`condition`, doctor) values (?, ?, ?, ?, ?, ?)");
+	        stmt.setInt(1, Integer.valueOf(textField.getText()));
+	        stmt.setString(2, textField_1.getText());
+	        stmt.setString(3, textField_2.getText());
+	        stmt.setString(4, textField_4.getText());
+	        stmt.setString(5, textField_3.getText());
+	        stmt.setString(6, selectedDoctor); // 선택된 의사 값을 바인딩합니다
+
+	        stmt.executeUpdate();
+
+	        // 예약 추가 후 테이블을 갱신합니다
+	        selectList(selectedDoctor);
+	        doctorSchedule(list2);
+
+	        // 예약 후 텍스트 필드들을 빈 칸으로 초기화
+	        textField.setText("");
+	        textField_1.setText("");
+	        textField_2.setText("");
+	        textField_3.setText("");
+	        textField_4.setText("");
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        DBUtil.close(stmt);
+	        DBUtil.close(conn);
+	    }
+	}
+
+
+   public void box() {
+       Connection conn = null;
+       PreparedStatement stmt = null;
+       ResultSet rs = null;
+
+       try {
+           conn = DBUtil.getConnection();
+           stmt = conn.prepareStatement("select name from staff where role = ?");
+           stmt.setString(1, "의사");
+           rs = stmt.executeQuery();
+           while (rs.next()) {
+               String name = rs.getString("name");
+               comboBox.addItem(name);
+           }
+       } catch (SQLException e) {
+           e.printStackTrace();
+       } finally {
+           DBUtil.close(rs);
+           DBUtil.close(stmt);
+           DBUtil.close(conn);
+       }
+   }
+     
+   
+   public StaffRegistration() {
+       JPanel one, two;
+       pane = new JTabbedPane();
+       pane.setBounds(0, 104, 984, 457);
+
+       one = new JPanel();
+       one.setLayout(null);
+       pane.addTab("선수목록", one);
+
+       two = new JPanel();
+       pane.addTab("예약하기", two);
+       two.setLayout(null);
+
+       // Create a JTable instance
+       table = new JTable();
+
+       // Create a DefaultTableModel
+       tableModel = new DefaultTableModel(
+           new Object[][]{},
+           new String[]{"\uB4F1\uBC88\uD638", "\uC120\uC218\uC774\uB984", "\uB0A0\uC9DC", "\uC2DC\uAC04", "\uBCD1\uBA85", "\uB2F4\uB2F9\uC758\uC0AC"}
+       );
+
+       // Set the table model to the table
+       table.setModel(tableModel);
+
+       // Create a JScrollPane and add the table to it
+       JScrollPane scrolledTable = new JScrollPane(table);
+       scrolledTable.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+       scrolledTable.setBounds(64, 38, 803, 221);
+       two.add(scrolledTable);
+       
+       // ===========================================================================================              
+       
+       comboBox = new JComboBox<>();
+       comboBox.setBounds(869, 10, 98, 21);
+       two.add(comboBox);
+       box();
+       comboBox.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String selectedDoctor = (String) comboBox.getSelectedItem();
+			selectList(selectedDoctor);
+			doctorSchedule(list2);
+		}
+	});
+
+       JLabel lblNewLabel_1 = new JLabel("의사");
+       lblNewLabel_1.setBounds(828, 13, 29, 15);
+       two.add(lblNewLabel_1);
+
+       comboBox_1 = new JComboBox<>();
+       comboBox_1.setBounds(147, 10, 98, 21);
+       two.add(comboBox_1);
+
+       comboBox_1.addActionListener(new ActionListener() {
+           
+		@Override
+           public void actionPerformed(ActionEvent e) {       	  
+			displaySchedule();
+               
+           }
+       });
+       
+       comboBox_1.addItemListener(new ItemListener() {
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			
+			System.out.println("값이 변경됨: " + e.getItem());
+		}
+	});
+
+       LocalDate currentDate = LocalDate.now();
+       LocalDate minusDate = currentDate.minusDays(15);
+       List<LocalDate> calendar = new ArrayList<>();
+       for (int i = 0; i < 30; i++) {
+           comboBox_1.addItem(minusDate.plusDays(i));
+       }
+       comboBox_1.setSelectedIndex(15);
+
+       JLabel lblNewLabel_2 = new JLabel("날짜");
+       lblNewLabel_2.setBounds(106, 13, 29, 15);
+       two.add(lblNewLabel_2);
+
+       JLabel lblNewLabel_4 = new JLabel("예약하기");
+       lblNewLabel_4.setBounds(41, 300, 57, 15);
+       two.add(lblNewLabel_4);
+
+       JLabel lblNewLabel_5 = new JLabel("등번호");
+       lblNewLabel_5.setBounds(41, 331, 57, 15);
+       two.add(lblNewLabel_5);
+
+       textField = new JTextField();
+       textField.setBounds(93, 328, 116, 21);
+       two.add(textField);
+       textField.setColumns(10);
+
+       JLabel lblNewLabel_6 = new JLabel("선수 이름");
+       lblNewLabel_6.setBounds(221, 331, 57, 15);
+       two.add(lblNewLabel_6);
+
+       textField_1 = new JTextField();
+       textField_1.setBounds(290, 328, 116, 21);
+       two.add(textField_1);
+       textField_1.setColumns(10);
+
+       JLabel lblNewLabel_7 = new JLabel("예약 날짜");
+       lblNewLabel_7.setBounds(418, 331, 57, 15);
+       two.add(lblNewLabel_7);
+
+       textField_2 = new JTextField();
+       textField_2.setBounds(484, 328, 116, 21);
+       two.add(textField_2);
+       textField_2.setColumns(10);
+
+       JLabel lblNewLabel_8 = new JLabel("병명");
+       lblNewLabel_8.setBounds(52, 379, 29, 15);
+       two.add(lblNewLabel_8);
+
+       textField_3 = new JTextField();
+       textField_3.setBounds(93, 376, 283, 21);
+       two.add(textField_3);
+       textField_3.setColumns(10);
+
+       JButton btnNewButton = new JButton("예약");
+       btnNewButton.setBounds(843, 367, 124, 38);
+       two.add(btnNewButton);      
+       
+       JLabel lblNewLabel_3 = new JLabel("시간");
+       lblNewLabel_3.setBounds(622, 331, 35, 15);
+       two.add(lblNewLabel_3);
+       
+       textField_4 = new JTextField();
+       textField_4.setBounds(669, 328, 116, 21);
+       two.add(textField_4);
+       textField_4.setColumns(10);
+       btnNewButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               insertAppointment();
+           }
+       });
+
+       pane.setSelectedIndex(0);
+       pane.addChangeListener(this);
+       getContentPane().setLayout(null);
+
+       JLabel label_1 = new JLabel();
+       label_1.setBounds(0, 0, 984, 0);
+       this.getContentPane().add(label_1);
+       this.getContentPane().add(pane);
+
+       lblNewLabel = new JLabel("New label");
+       lblNewLabel.setIcon(new ImageIcon(StaffRegistration.class.getResource("/image/선수위-배경.jpg")));
+       lblNewLabel.setBounds(0, 0, 984, 104);
+       getContentPane().add(lblNewLabel);
+
+       this.setSize(1000, 600);
+       this.setVisible(true);
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+
+   @Override
+   public void stateChanged(ChangeEvent e) {
+       int index = pane.getSelectedIndex();// 현재탭의 번호를 가져온다
+       String msg = pane.getTitleAt(index); // index 위에 탭 문자열을 가져옴
+       msg += "탭이 선택되었습니다";
+
+       pane.setSelectedIndex(index); // 현재 선택한 탭으로 화면 출력 변경
+   }
+
+   public static void main(String[] args) {
+       new StaffRegistration();
+   }
 }
